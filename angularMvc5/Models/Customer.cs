c832d13f4f600f83ecd322102e319586d0b07649
Sheryar <supersheryarshah@gmail.com>
2019-08-16 20:15:07 +0545

    Added customer model

--- angularMvc5/Models/Customer.cs
+++ angularMvc5/Models/Customer.cs
@@ -7,5 +7,21 @@ namespace angularMvc5.Models
 {
     public class Customer
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+        public Customer(DataRow row)
+        {
+            this.Id = Convert.ToInt32(row["Id"]);
+            this.FirstName = row["FirstName"].ToString();
+            this.LastName = row["LastName"].ToString();
+            this.City = row["City"].ToString();
+            this.Country = row["Country"].ToString();
+            this.Phone = row["Phone"].ToString();
+        }
     }
 }
