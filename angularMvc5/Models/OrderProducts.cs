2e73688e3edb3aa1328d6371b761b2d2899bf57c
Sheryar <supersheryarshah@gmail.com>
2019-08-16 20:15:35 +0545

    Added OrderProducts model

--- angularMvc5/Models/OrderProducts.cs
+++ angularMvc5/Models/OrderProducts.cs
@@ -7,5 +7,20 @@ namespace angularMvc5.Models
 {
     public class OrderProducts
     {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+
+        public Product Product { get; set; }
+        public Order Order { get; set; }
+
+        public OrderProducts(DataRow row)
+        {
+            this.OrderId = Convert.ToInt32(row["OrderId"]);
+            this.ProductId = Convert.ToInt32(row["ProductId"]);
+            this.UnitPrice = Convert.ToDouble(row["UnitPrice"]);
+            this.Quantity = Convert.ToDouble(row["Quantity"]);
+        }
     }
 }
