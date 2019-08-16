d961b842c129817406c2e4707b61dbe389c3bb98
Sheryar <supersheryarshah@gmail.com>
2019-08-16 20:45:21 +0545

    Added Datalayer Controller

---
+++ angularMvc5/Controllers/Core/DataLayerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace angularMvc5.Controllers.Core
+{
+    public class DataLayerController : Controller
+    {
+        private const string connectionString = "data source=(localdb)\\MSSQLLocalDB; initial catalog=MyDB;Integrated Security=True";
+
+        public DataTable Select(string storedProcedureorCommandText, bool isStoredProcedure = true)
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    if (!isStoredProcedure)
+                    {
+                        command.CommandType = CommandType.Text;
+                    }
+                    command.CommandText = storedProcedureorCommandText;
+                    connection.Open();
+
+                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+
+        }
+    }
+}
