e27f5463dc9254842fd5e5c3bd1eaee36dcbf313
Sheryar <supersheryarshah@gmail.com>
2019-08-16 20:15:19 +0545

    Added Order Model

--- angularMvc5/Models/Order.cs
+++ angularMvc5/Models/Order.cs
@@ -7,6 +7,21 @@ namespace angularMvc5.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int OrderNumber { get; set; }
+        public int CustomerId { get; set; }
+        public double TotalAmount { get; set; }
 
+        public Customer Customer { get; set; }
+
+        public Order(DataRow row)
+        {
+            this.Id = Convert.ToInt32(row["Id"]);
+            this.OrderDate = Convert.ToDateTime(row["OrderDate"]);
+            this.OrderNumber = Convert.ToInt32(row["OrderNumber"]);
+            this.CustomerId = Convert.ToInt32(row["CustomerId"]);
+            this.TotalAmount = Convert.ToDouble(row["TotalAmount"]);
+        }
     }
 }
